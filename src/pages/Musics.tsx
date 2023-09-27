49cdec888eecd20bd2077b5fbe63a0b75d8cb04e
GodaHaruki <102050420+GodaHaruki@users.noreply.github.com>
2023-09-27 12:53:48 +0900

    1 file modified

--- src/pages/Musics.tsx
+++ src/pages/Musics.tsx
@@ -1,15 +1,23 @@
 import { Music, MusicCard } from "../components/MusicCard"
+import { Grid } from "../design/Grid"
 
 export const Musics: React.FC<{musics: Music[]}>
 = ({musics}) => {
   return (
-    <>
-    All Musics
+    <Grid>
+      <div className="col-span-9">
+        All music
+      </div>
+      <div className="col-span-3">
+        <input type="file" className="" />
+      </div>
+    <div className="col-span-full">
     {
     musics.map(m => 
     <MusicCard music={m} />
     )
     }
-    </>
+    </div>
+    </Grid>
   )
 }
