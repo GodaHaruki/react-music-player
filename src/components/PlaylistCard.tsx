1de73598a089f89912119bb2483bd3e1c1011858
GodaHaruki <102050420+GodaHaruki@users.noreply.github.com>
2023-09-27 12:41:45 +0900

    1 file modified

--- src/components/PlaylistCard.tsx
+++ src/components/PlaylistCard.tsx
@@ -3,8 +3,6 @@ import { Music } from "./MusicCard"
 export interface Playlist {
   name: string,
   musics: Music[]
-
-  imgUrl?: string
 }
 
 export const PlaylistCard: React.FC<{playlist: Playlist}>
