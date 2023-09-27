9fc0d348b094b4174bff99700d8c4212de143a08
GodaHaruki <102050420+GodaHaruki@users.noreply.github.com>
2023-09-27 12:57:46 +0900

    1 file modified

--- src/pages/Musics.tsx
+++ src/pages/Musics.tsx
@@ -1,6 +1,8 @@
 import { Music, MusicCard } from "../components/MusicCard"
 import { Grid } from "../design/Grid"
 
+const handleFileInput = (fl: FileList) => {}
+
 export const Musics: React.FC<{musics: Music[]}>
 = ({musics}) => {
   return (
@@ -9,7 +11,7 @@ export const Musics: React.FC<{musics: Music[]}>
         All music
       </div>
       <div className="col-span-3">
-        <input type="file" className="" />
+        <input type="file" className="" onChange={(evt) => handleFileInput(evt.target.files)}/>
       </div>
     <div className="col-span-full">
     {
